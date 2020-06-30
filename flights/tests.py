e6157a9ddd790a553900242d4828efedf31e5671
nkessilfife <essilfien18@gmail.com>
2020-06-30 11:52:17 +0000

    commit 2

--- flights/tests.py
+++ flights/tests.py
@@ -1,6 +1,7 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.db.models import Max
 
-from .models import Airport, Flight
+from .models import Airport, Flight, Passenger
 
 # Create your tests here.
 class ModelsTestCase(TestCase):
@@ -40,3 +41,43 @@ class ModelsTestCase(TestCase):
         a2 = Airport.objects.get(code="BBB")
         f = Flight.objects.get(origin=a1, destination=a2, duration=-100)
         self.assertFalse(f.is_valid_flight())
+
+    def test_index(self):
+        c = Client()
+        response = c.get("/")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context["flights"].count(), 3)
+
+    def test_valid_flight_page(self):
+        a1 = Airport.objects.get(code="AAA")
+        f = Flight.objects.get(origin=a1, destination=a1)
+
+        c = Client()
+        response = c.get(f"/{f.id}")
+        self.assertEqual(response.status_code, 200)
+
+    def test_invalid_flight_page(self):
+        max_id = Flight.objects.all().aggregate(Max("id"))["id__max"]
+
+        c = Client()
+        response = c.get(f"/{max_id + 1}")
+        self.assertEqual(response.status_code, 404)
+
+    def test_flight_page_passengers(self):
+        f = Flight.objects.get(pk=1)
+        p = Passenger.objects.create(first="Alice", last="Adams")
+        f.passengers.add(p)
+
+        c = Client()
+        response = c.get(f"/{f.id}")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context["passengers"].count(), 1)
+
+    def test_flight_page_non_passengers(self):
+        f = Flight.objects.get(pk=1)
+        p = Passenger.objects.create(first="Alice", last="Adams")
+
+        c = Client()
+        response = c.get(f"/{f.id}")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context["non_passengers"].count(), 1)
