609a791043d61d8ba0000160a0d2edb948fd9bab
nkessilfife <essilfien18@gmail.com>
2020-06-30 16:00:49 +0000

    made chnages to the base_dir

--- airlines/settings.py
+++ airlines/settings.py
@@ -13,9 +13,9 @@ https://docs.djangoproject.com/en/dev/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-#BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
-BASE_DIR = Path.cwd()
-DATABASES = str(BASE_DIR / "db.sqlite3")
+BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
+#BASE_DIR = Path.cwd()
+#DATABASES = str(BASE_DIR / "db.sqlite3")
 
 
 # Quick-start development settings - unsuitable for production
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'airlines.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
